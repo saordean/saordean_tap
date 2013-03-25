3de7ab9a8773fb5d779bfb478ba385906edf0bed
Jerry Taylor Kendrick <saordean@Geralds-MacBook-Pro.local>
2013-03-25 18:05:13 -0500

    Initial Commit

---
+++ saordean_tap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  saordean_tap
+//
+//  Created by JerryTaylorKendrick on 3/25/13.
+//  Copyright (c) 2013 Gerald Dean Taylor Kendrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ saordean_tap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  saordean_tap
+//
+//  Created by JerryTaylorKendrick on 3/25/13.
+//  Copyright (c) 2013 Gerald Dean Taylor Kendrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
